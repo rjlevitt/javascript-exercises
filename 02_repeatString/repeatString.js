8c7eaaefff8d87fb383fd2c0ebb8cf8efd083e6f
vulpes37 <rjlevitt37@gmail.com>
2022-03-10 17:00:38 -0700

    first three exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeat_string = "";
+    let i = 0;
+    if(num<0){
+        return "ERROR"
+    }else{
+        while(i<num){
+            repeat_string += string;
+            i++;
+        }
+        return repeat_string
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    new_string = "";
+    for(i=string.length; i>0; i--){
+        new_string += string.slice(i-1, i);
+    }
+    return new_string
 };
 
 // Do not edit below this line
