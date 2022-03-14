765e6c909a568046b195c8d5da7de54af148a252
vulpes37 <rjlevitt37@gmail.com>
2022-03-14 17:03:50 -0600

    solutions 4 5 6

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const inputList = args[0];
+    const outputList = [];
 
+    inputList.forEach((item) => {
+        if (!args.includes(item)) {
+            outputList.push(item);
+        }
+    });
+    return outputList
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    if(Number.isInteger(start) && Number.isInteger(end)){
+        if(start >= 0 && end >= 0){
+            if(start > end){
+                const temp = start;
+                start = end;
+                end = temp;
+            }
+            for(i=start; i<=end; i++){
+                sum+=i
+            }
+            return sum
+        }
+        else{
+            return "ERROR" 
+        }
+    }
+    else{
+        return "ERROR"
+    }
 
 };
 
