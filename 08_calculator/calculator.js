09d20bc668b5bc7f948e0835c47abbf3b3893201
vulpes37 <rjlevitt37@gmail.com>
2022-04-09 07:04:25 -0600

    calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(val_one, val_two) {
+  return val_one + val_two
 };
 
-const subtract = function() {
-	
+const subtract = function(val_one, val_two) {
+	return val_one - val_two
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce( (x1, x2) => x1 + x2, 0)
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  if(arr.length){
+    return arr.reduce( (x1, x2) => x1 * x2 )
+    
+  }else{
+    return 0
+  }
 
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  powerArray = Array(power).fill(num)
+  return powerArray.reduce( (x1, x2) => x1 * x2 )
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  let product = 1;
+  for (let i = num; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
