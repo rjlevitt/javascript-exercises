8ef2f3896bb76ad88651c64dc7a276ca2bcf7e30
vulpes37 <rjlevitt37@gmail.com>
2022-10-26 09:13:55 -0600

    finished fib and map exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = Number(number)
+    if( number >= 0){
+        let n1 = 0;
+        let n2 = 1;
+        let nextNumber;
+        for(i=1; i<=number; i++){
+            nextNumber = n1 + n2;
+            n1 = n2;
+            n2 = nextNumber;
+        }
+        return n1;
+    }
+    else{
+        return "OOPS"
+    }
+
 
 };
 
